0520b06a0b051c343aca4fa68fbb57cc6ba51140
Carlos Eduardo Nicioli de Toledo <111098632+eduardonicioli@users.noreply.github.com>
2023-11-07 21:19:22 -0300

    add bootstrap
    adm-faq
    adm-index
    adm-inserir

--- adm-faq.php
+++ adm-faq.php
@@ -21,37 +21,45 @@ try {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Administraivo FAQ</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Administrativo FAQ</title>
+  
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
 </head>
 <body>
-    <h1>Administraivo FAQ</h1>
-    <h3>Lista de Mensagens enviadas ao FAQ</h3>
-    <table border="1">
-        <tr>
-            <th>Id Mensagem</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Assunto</th>
-            <th>Mensagem</th>
-            <th>Ações</th>
-        </tr>
-        <?php foreach ($mensagens as $mensagem): ?>
-        <tr>
-            <td><?php echo $mensagem['id_mensagem'] ?></td>
-            <td><?php echo $mensagem['nome'] ?></td>
-            <td><?php echo $mensagem['email'] ?></td>
-            <td><?php echo $mensagem['assunto'] ?></td>
-            <td><?php echo $mensagem['mensagem'] ?></td>
-            <td>
-                <a href="#">Atualizar</a>
-                <a href="excluir-mensagem.php">Excluir</a>
-            </td>
-        </tr>
-        <?php endforeach ?>
-    </table>
-    <br><br>
-    <a href="adm-inserir.php">Adicionar administrador.</a><br><br>
-    <a href="index.html">Home
-    </a><br><br>
+    <div class="container">
+        <h1 class="mt-4">Administrativo FAQ</h1>
+        <h3>Lista de Mensagens enviadas ao FAQ</h3>
+        <table class="table table-bordered">
+            <tr>
+                <th>Id Mensagem</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Assunto</th>
+                <th>Mensagem</th>
+                <th>Ações</th>
+            </tr>
+            <?php foreach ($mensagens as $mensagem): ?>
+            <tr>
+                <td><?php echo $mensagem['id_mensagem'] ?></td>
+                <td><?php echo $mensagem['nome'] ?></td>
+                <td><?php echo $mensagem['email'] ?></td>
+                <td><?php echo $mensagem['assunto'] ?></td>
+                <td><?php echo $mensagem['mensagem'] ?></td>
+                <td>
+                    <a href="#">Atualizar</a>
+                    <a href="">Excluir</a>
+                </td>
+            </tr>
+            <?php endforeach ?>
+        </table>
+        <br><br>
+        <a href="adm-inserir.php" class="btn btn-primary">Adicionar administrador</a><br><br>
+        <a href="index.html" class="btn btn-secondary">Home</a><br><br>
+    </div>
+
 </body>
 </html>
--- adm-inserir.php
+++ adm-inserir.php
@@ -2,21 +2,34 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>Cadastro Administrador</title>
+  
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
 </head>
 <body>
-    
-<h3>Novo Administrador</h3>
-<form action="adm-gravar.php" method="post">
-    <label for="usuario">Usuário:</label>
-    <input type="text" name="usuario">
-    <br><br>
-    <label for="senha">Senha:</label>
-    <input type="password" name="senha">
-    <br><br>
-    <input type="submit" value="Gravar">
-</form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card-body">
+                <h3 class="col md-4">Novo Administrador</h3>
+                    <form action="adm-gravar.php" method="post">
+                        <div class="form-group">
+                            <label for="usuario">Usuário:</label>
+                            <input type="text" class="form-control" name="usuario">
+                        </div>
+                        <div class="form-group">
+                            <label for="senha">Senha:</label>
+                            <input type="password" class="form-control" name="senha">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Gravar</button>
+                    </form>
+            </div>
+            
+        </div>
+    </div>
+        
 
 </body>
 </html>
